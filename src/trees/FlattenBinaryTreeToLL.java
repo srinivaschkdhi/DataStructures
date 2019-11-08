71e8ee7b2513cef31b208b6bbefeddf69c13f962
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-08 18:45:54 +0530

    Create FlattenBinaryTreeToLL.java

---
+++ src/trees/FlattenBinaryTreeToLL.java
@@ -0,0 +1,43 @@
+package trees;
+
+import java.util.Stack;
+
+public class FlattenBinaryTreeToLL {
+
+    public static void flatten(Node root) {
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+
+        while (!stack.isEmpty()) {
+            Node current_node = stack.pop();
+
+            if (current_node.right != null) stack.push(current_node.right);
+
+            if (current_node.left != null) stack.push(current_node.left);
+
+            if (!stack.isEmpty()) current_node.right = stack.peek();
+
+            current_node.left = null;
+        }
+
+        print(root);
+    }
+
+    public  static void print(Node root){
+        Node current = root;
+        while(current != null){
+            System.out.println(current.key);
+            current = current.right;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Node rootNode = new Node(1);
+        rootNode.left = new Node(2);
+        rootNode.right = new Node(3);
+        rootNode.left.left = new Node(4);
+        rootNode.left.right = new Node(5);
+        flatten(rootNode);
+    }
+}
