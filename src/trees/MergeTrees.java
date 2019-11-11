d9e268c44ceaf686ab8935be1f4081a371518ef0
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-11 22:48:00 +0530

    Merge Two Binary Trees

---
+++ src/trees/MergeTrees.java
@@ -0,0 +1,18 @@
+class MergeTrees {
+    public Node mergeTrees(Node t1, Node t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+
+        t1.key = t1.key + t2.key;
+
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+
+        return t1;
+    }
+
+}
