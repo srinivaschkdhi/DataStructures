80b69657798f846b776fd5a4742446c36529793d
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2019-11-13 22:52:38 +0530

    Create PartitionList.java

---
+++ src/linkedlist/singlylinkedlist/PartitionList.java
@@ -0,0 +1,29 @@
+
+package linkedlist.singlylinkedlist;
+
+
+public class PartitionList {
+    public ListNode partition(ListNode head, int x) {
+        ListNode bfHead = new ListNode(0);
+        ListNode bf = bfHead;
+
+        ListNode aftrHead = new ListNode(0);
+        ListNode aftr = aftrHead;
+
+        while (head != null) {
+            if (head.data < x) {
+                bf.next = head;
+                bf = bf.next;
+            } else {
+                aftr.next = head;
+                aftr = aftr.next;
+            }
+            head = head.next;
+        }
+
+        aftr.next = null;
+        bf.next = aftrHead.next;
+
+        return bfHead.next;
+    }
+}
