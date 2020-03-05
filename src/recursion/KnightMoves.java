6a9c52de80a87b9d40fdd4a1a671b925fc13a726
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-03-05 23:34:34 +0530

    Refactored KnightMoves

--- src/recursion/KnightMoves.java
+++ src/recursion/KnightMoves.java
@@ -5,40 +5,39 @@
 package recursion;
 
 public class KnightMoves {
-    private static boolean canPlace(int board[][],int n,int r,int c){
+    private static boolean canPlace(int board[][], int n, int r, int c) {
         return
-                r >=0 && r < n &&
-                c >=0 && c < n &&
-                board[r][c] == 0;
+                r >= 0 && r < n &&
+                        c >= 0 && c < n &&
+                        board[r][c] == 0;
     }
 
-    static boolean  solveKnightMove(int[][] board,int n,int move_no,int curRow,int curCol){
-        if(move_no == n * n)
+    static boolean solveKnightMove(int[][] board, int n, int move_no, int sr, int sc) {
+        if (move_no == n * n)
             return true;
 
-        int[] rowDir = {2,1,-1,-2,-2,-1,1,2};
-        int[] colDir = {1,2,2,1,-1,-2,-2,-1};
+        int[] dr = {2, 1, -1, -2, -2, -1, 1, 2};
+        int[] dc = {1, 2, 2, 1, -1, -2, -2, -1};
 
-        for(int curDir = 0;curDir < n;curDir++){
-            int nextRow = curRow  + rowDir[curDir];
-            int nextCol = curCol + colDir[curDir];
+        for (int i = 0; i < n; i++) {
+            int r = sr + dr[i];
+            int c = sc + dc[i];
 
-            if(canPlace(board,n,nextRow,nextCol)){
-                board[nextRow][nextCol] = move_no + 1;
-                boolean isSucessful = solveKnightMove(board,n,move_no+1,nextRow,nextCol);
+            if (canPlace(board, n, r, c)) {
+                board[r][c] = move_no + 1;
+                boolean isSuccess = solveKnightMove(board, n, move_no + 1, r, c);
 
-                if(isSucessful == true) return true;
-
-                board[nextRow][nextCol] = 0;
+                if (isSuccess == true) return true;
+                board[r][c] = 0;
             }
         }
         return false;
     }
 
-    static void printBoard(int[][] board,int n){
+    static void printBoard(int[][] board, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(board[i][j]+"     ");
+                System.out.print(board[i][j] + "     ");
             }
             System.out.println();
         }
@@ -49,10 +48,10 @@ public class KnightMoves {
         int[][] board = new int[n][n];
         board[0][0] = 1;
 
-        boolean answer = solveKnightMove(board,n,1,0,0);
+        boolean answer = solveKnightMove(board, n, 1, 0, 0);
 
-        if(answer == true)
-            printBoard(board,n);
+        if (answer == true)
+            printBoard(board, n);
         else
             System.out.println("Sorry Man! Can't Visit the board");
     }
