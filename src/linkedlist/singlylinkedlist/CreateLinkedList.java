a27b5d0a6e550a10bc51176931ebc024272babdc
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-03-07 00:09:22 +0530

    Delete Largest Element in Linked List

--- src/linkedlist/singlylinkedlist/CreateLinkedList.java
+++ src/linkedlist/singlylinkedlist/CreateLinkedList.java
@@ -3,12 +3,14 @@ package linkedlist.singlylinkedlist;
 public class CreateLinkedList {
 
     public ListNode invoke() {
-        ListNode firstNode = new ListNode(1);
-        ListNode secondNode = new ListNode(2);
-        ListNode fourthNode = new ListNode(4);
+        ListNode firstNode = new ListNode(1000);
+        ListNode secondNode = new ListNode(21000);
+        ListNode thirdNode = new ListNode(21);
+        ListNode fourthNode = new ListNode(400);
 
         firstNode.next = secondNode;
-        secondNode.next = fourthNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = fourthNode;
 
         return firstNode;
     }
