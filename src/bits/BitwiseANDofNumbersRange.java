6cb265aa92416d48fbebd81ac3618d66f9df7666
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-05-16 09:06:09 +0530

    Create BitwiseANDofNumbersRange.java

---
+++ src/bits/BitwiseANDofNumbersRange.java
@@ -0,0 +1,12 @@
+public class BitwiseANDofNumbersRange {
+  public int rangeBitwiseAnd(int m, int n) {
+        int count = 0;
+        
+        while(m != n){
+            m >>= 1;
+            n >>= 1;
+            count++;
+        }
+         return m <<=count;
+    }
+}    
